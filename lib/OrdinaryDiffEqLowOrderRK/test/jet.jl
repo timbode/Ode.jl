93eb57788730c0cd6bd1be43b57417182e94bdf8
timbode <lappet@protonmail.com>
2025-07-07 11:05:48 +0200

    remove all non-RK4 stuff

--- lib/OrdinaryDiffEqLowOrderRK/test/jet.jl
+++
@@ -1,8 +0,0 @@
-import OrdinaryDiffEqLowOrderRK
-using JET
-
-# False positive typo
-# @testset "JET Tests" begin
-#     test_package(
-#         OrdinaryDiffEqLowOrderRK, target_defined_modules = true, mode = :typo)
-# end
